6915b9b7d7e19211a748f13717f03c5d3923072e
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-07-06 20:32:08 +0500

    Add project
    add project

---
+++ need.py
@@ -0,0 +1,47 @@
+
+region = [
+    {
+        'name': 'Andijan',
+
+    },
+    {
+        'name': 'Namangan',
+    },
+    {
+        'name': 'Fergana',
+    },
+    {
+        'name': 'Tashkent',
+    },
+    {
+        'name': 'Guliston',
+    },
+    {
+        'name': 'Jizzakh',
+    },
+    {
+        'name': 'Samarkand',
+    },
+    {
+        'name': 'Navoiy',
+    },
+    {
+        'name': 'Bukhara',
+    },
+    {
+        'name': 'Qarshi',
+    },
+    {
+        'name': 'Termiz',
+    },
+    {
+        'name': 'Urganch',
+    },
+    {
+        'name': 'Nukus',
+    }
+]
+
+for reg in region:
+    print(reg['name'])
+
